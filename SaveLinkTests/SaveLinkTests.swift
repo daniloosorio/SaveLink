a7575880141b128237b3e9a8db6767f7cf6a9da0
Danilo Osorio <odanilo38@gmail.com>
2025-04-10 20:07:12 -0500

    Initial Commit

---
+++ SaveLinkTests/SaveLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinkTests.swift
+//  SaveLinkTests
+//
+//  Created by Danilo Osorio on 10/04/25.
+//
+
+import Testing
+@testable import SaveLink
+
+struct SaveLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
